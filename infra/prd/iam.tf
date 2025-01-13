db0fe89cc494bb195510ed41db6f762ac66e6c4a
shomakobayashi <pipipipiii0001@yahoo.co.jp>
2025-01-14 02:05:56 +0900

    iam修正_全てのブランチ許可

--- infra/prd/iam.tf
+++ infra/prd/iam.tf
@@ -57,8 +57,7 @@ resource "aws_iam_role" "github_actions_role" {
         },
         Action    = "sts:AssumeRoleWithWebIdentity",
         Condition = {
-          StringEquals = {
-            "token.actions.githubusercontent.com:aud": "sts.amazonaws.com",
+          "StringLike": {
             "token.actions.githubusercontent.com:sub": "repo:shomakobayashi/todo-list-app:*"
           }
         }
